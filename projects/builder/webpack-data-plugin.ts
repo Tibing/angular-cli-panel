eff8c1e5cf2810807d6398b36d2e29ccbef97426
Alexander Demeshko <demeshko.alexander.v@gmail.com>
2019-07-24 19:05:23 +0300

    feat(plugin): connect webpack data

--- projects/builder/webpack-data-plugin.ts
+++ projects/builder/webpack-data-plugin.ts
@@ -1,13 +1,54 @@
-import { interval, Subject } from 'rxjs';
-import { Event } from '@cli-panel/panel';
+import { Subject } from 'rxjs';
+import { ProgressPlugin } from 'webpack';
+
+// import { Event } from '@cli-panel/panel';
+
+interface Event {
+  type: string;
+  payload: any;
+}
 
 export class WebpackDataPlugin {
-  constructor(eventBus: Subject<Event>) {
-    interval(100).subscribe(value => {
-      eventBus.next({ type: 'progress', payload: value });
-    });
+
+  constructor(private eventBus: Subject<Event>) {
   }
 
-  apply() {
+  apply(compiler) {
+    new ProgressPlugin((percent: number, info: string, ...args) => {
+
+      this.sendData({
+        type: 'progress',
+        payload: { percent, info, additional: args },
+      });
+    })
+      .apply(compiler);
+
+    compiler.hooks.run.tapAsync('PanelPlugin', (done) => {
+      // this.sendData('RUN');
+      done();
+    });
+
+    compiler.hooks.compile.tap('PanelPlugin', () => {
+      // this.sendData('COMPILING');
+    });
+
+    compiler.hooks.invalid.tap('PanelPlugin', () => {
+      // this.sendData('INVALID');
+    });
+
+    compiler.hooks.failed.tap('PanelPlugin', () => {
+      // this.sendData('FAILED');
+    });
+
+    compiler.hooks.done.tapAsync('PanelPlugin', (stats) => {
+      // this.sendData(`DONE in ${stats.endTime - stats.startTime}ms`);
+      // this.sendData(`ERRORS=[${stats.compilation.errors}]`);
+      // this.sendData(`WARNINGS=[${stats.compilation.warnings}]`);
+    });
+
+  }
+
+  sendData(event: Event) {
+    this.eventBus.next(event);
   }
 }
