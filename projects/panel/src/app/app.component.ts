3857d46d07ff90f7a7fa0e31bd88a36960619bd2
Tibing <nik.poltoratsky@gmail.com>
2019-07-29 20:53:09 +0300

    feat(panel): status colorization

--- projects/panel/src/app/app.component.ts
+++ projects/panel/src/app/app.component.ts
@@ -4,16 +4,18 @@ import { map, startWith } from 'rxjs/operators';
 
 import { DataFacade } from './data/data.facade';
 import { formatModules } from './util/format-modules';
+import { StatusPayload } from './event-bus';
 
 @Component({
   selector: 'cp-root',
   template: `
     <box
       [label]="'Status'"
-      [content]="status$ | async"
+      [content]="statusStyled$ | async"
       width="20%"
       height="3"
-      [style]="{ fg: -1, border: { fg: 'green' } }">
+      [style]="commonStyle$ | async"
+    >
     </box>
 
     <box
@@ -22,16 +24,11 @@ import { formatModules } from './util/format-modules';
       width="20%"
       height="3"
       left="20%"
-      [style]="{ fg: -1, border: { fg: 'green' } }">
+      [style]="commonStyle$ | async"
+    >
     </box>
 
-    <box
-      label="Progress"
-      width="60%"
-      height="3"
-      left="40%"
-      [style]="{ fg: -1, border: { fg: 'green' } }">
-
+    <box label="Progress" width="60%" height="3" left="40%" [style]="commonStyle$ | async">
       <progressbar
         [value]="progress$ | async"
         width="100%-4"
@@ -39,20 +36,13 @@ import { formatModules } from './util/format-modules';
         top="center"
         left="center"
         orientation="horizontal"
-        [style]="{ bar: { bg: 'green'}}">
+        [style]="progressBarStyle$ | async"
+      >
       </progressbar>
-
     </box>
 
-    <box
-      width="100%"
-      height="50%"
-      label="Log"
-      top="3"
-      [style]="{ fg: -1, border: { fg: 'green' } }">
-
-      <log [log]="log$ | async" width="100%-5"></log>
-
+    <box width="100%" height="50%" label="Log" top="3" [style]="commonStyle$ | async">
+      <log left="1" width="100%-5" [log]="log$ | async"></log>
     </box>
 
     <listbar
@@ -61,11 +51,10 @@ import { formatModules } from './util/format-modules';
       width="50%"
       height="50%-3"
       top="50%+3"
-      [style]="modulesStyle"
+      [style]="modulesStyle$ | async"
       [autoCommandKeys]="true"
       [commands]="commands$ | async"
     >
-
       <table
         height="100%-5"
         width="100%-5"
@@ -73,9 +62,7 @@ import { formatModules } from './util/format-modules';
         [data]="modules$ | async"
         top="1"
         left="1"
-      >
-      </table>
-
+      ></table>
     </listbar>
 
     <box
@@ -84,46 +71,61 @@ import { formatModules } from './util/format-modules';
       height="50%-3"
       top="50%+3"
       left="50%"
-      [style]="{ fg: -1, border: { fg: 'green' } }"
+      [style]="commonStyle$ | async"
     >
-
-      <table
-        height="100%-5"
-        width="100%-5"
-        top="1"
-        align="left"
-        [data]="assets$ | async"
-      >
-      </table>
-
+      <table height="100%-5" width="100%-5" top="1" align="left" [data]="assets$ | async"></table>
     </box>
   `,
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-
   progress$: Observable<number> = this.dataFacade.progress$;
 
-  log$: Observable<string> = this.dataFacade.log$;
+  log$: Observable<string> = this.dataFacade.log$.pipe(
+    startWith('Compiling...'),
+  );
 
-  status$: Observable<string> = this.dataFacade.status$.pipe(
-    map((status: string) => `{green-fg}{bold}${status}{/}`),
+  status$: Observable<string> = this.dataFacade.status$;
+
+  statusStyled$: Observable<string> = this.dataFacade.status$.pipe(
+    map((status: StatusPayload) => {
+      const color = this.statusToColor(status);
+      return `{${color}-fg}{bold}${status}{/}`;
+    })
   );
 
   assets$: Observable<any> = this.dataFacade.assets$.pipe(
-    startWith([['Waiting for the end of the compilation...']]),
+    startWith([['Waiting for the end of the compilation...']])
   );
-  modulesStyle = {
-    fg: -1,
-    border: { fg: 'green' },
-    prefix: { fg: -1 },
-    item: { fg: 'white' },
-    selected: { fg: 'black', bg: 'green' },
-  };
-  private modules: Subject<any> = new Subject();
-  modules$: Observable<any> = this.modules.asObservable().pipe(
-    startWith([['Waiting for the end of the compilation...']]),
+
+  commonStyle$: Observable<any> = this.status$.pipe(
+    startWith('compiling'),
+    map((status: StatusPayload) => {
+      const color = this.statusToColor(status);
+      return this.commonStyleWithColor(color);
+    })
   );
+
+  modulesStyle$: Observable<any> = this.status$.pipe(
+    startWith('compiling'),
+    map((status: StatusPayload) => {
+      const color = this.statusToColor(status);
+      return this.modulesStyleWithColor(color);
+    })
+  );
+
+  progressBarStyle$: Observable<any> = this.status$.pipe(
+    startWith('compiling'),
+    map((status: StatusPayload) => {
+      const color = this.statusToColor(status);
+      return this.progressStyleWithColor(color);
+    })
+  );
+
+  modules: Subject<any> = new Subject();
+  modules$: Observable<any> = this.modules
+    .asObservable()
+    .pipe(startWith([['Waiting for the end of the compilation...']]));
   commands$: Observable<any> = this.dataFacade.sizes$.pipe(
     map(sizes => sizes.value.assets),
     map(assets => {
@@ -132,14 +134,54 @@ export class AppComponent {
           Object.assign({}, memo, {
             [name]: () => {
               this.modules.next(formatModules(assets[name].files));
-            },
+            }
           }),
-        {},
+        {}
       );
     }),
-    startWith([]),
+    startWith([])
   );
 
-  constructor(private dataFacade: DataFacade) {
+  constructor(private dataFacade: DataFacade) {}
+
+  private commonStyleWithColor(color: string = 'white') {
+    return { fg: -1, border: { fg: color } };
+  }
+
+  private progressStyleWithColor(color: string = 'white') {
+    return { bar: { bg: color } };
+  }
+
+  private modulesStyleWithColor(color: string = 'white') {
+    return {
+      fg: -1,
+      border: { fg: color },
+      prefix: { fg: -1 },
+      item: { fg: 'white' },
+      selected: { fg: 'black', bg: color }
+    };
+  }
+
+  private statusToColor(status: StatusPayload): string {
+    const defaultColor = 'white';
+    let color = defaultColor;
+
+    if (status === 'compiling') {
+      color = defaultColor;
+    }
+
+    if (status === 'invalidated') {
+      color = defaultColor;
+    }
+
+    if (status === 'success') {
+      color = 'green';
+    }
+
+    if (status === 'failed') {
+      color = 'red';
+    }
+
+    return color;
   }
 }
--- projects/panel/src/app/data/socket.ts
+++ projects/panel/src/app/data/socket.ts
@@ -1,10 +1,10 @@
 import { ApplicationRef, Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
+import { combineLatest, Observable, Subject } from 'rxjs';
+import { map, mergeMap, startWith } from 'rxjs/operators';
 
 import { Event, EventBus, OperationEvent, ProgressPayload, StatusPayload } from '../event-bus';
 import { formatAssets } from '../util/format-assets';
 import { parseProblems, parseSizes } from './stats-parser';
-import { map, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class Socket {
@@ -12,8 +12,6 @@ export class Socket {
   private log = new Subject<string>();
   log$: Observable<string> = this.log.asObservable();
 
-  private status = new Subject<StatusPayload>();
-  status$: Observable<StatusPayload> = this.status.asObservable();
 
   private operation = new Subject<OperationEvent>();
   operation$: Observable<OperationEvent> = this.operation.asObservable();
@@ -28,6 +26,25 @@ export class Socket {
   sizes$: Observable<any> = this.stats$.pipe(
     mergeMap(parseSizes),
   );
+
+  private hasErrors$: Observable<boolean> = this.stats$.pipe(
+    map(data => data.errors),
+  );
+
+  private status = new Subject<StatusPayload>();
+  status$: Observable<StatusPayload> = combineLatest([
+    this.status.asObservable(),
+    this.hasErrors$.pipe(startWith(false)),
+  ])
+    .pipe(
+      map(([status, hasErrors]) => {
+        if (hasErrors) {
+          return 'failed';
+        } else {
+          return status;
+        }
+      }),
+    );
 
   assets$: Observable<any> = this.sizes$.pipe(
     map(sizes => sizes.value.assets),
